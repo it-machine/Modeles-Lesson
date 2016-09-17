e228fdc081f25626cc3e3c4c82369c071dc967c8
Artem <artemsterkh@yandex.ru>
2016-09-17 14:56:59 +0300

    Initial Commit

---
+++ Modules Lesson/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Modules Lesson
+//
+//  Created by Artem Konarev on 17.09.16.
+//  Copyright © 2016 it-machine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
